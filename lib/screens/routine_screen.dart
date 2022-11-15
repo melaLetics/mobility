b097fac25f327429fb75f7c2ffcb1ad84beee18b
mschick <mschick@novomind.com>
2022-11-15 08:06:55 +0100

    plugins

--- lib/screens/routine_screen.dart
+++ lib/screens/routine_screen.dart
@@ -1,14 +1,24 @@
 import 'package:flutter/material.dart';
 import 'package:mobility/model/exercise.dart';
 
-class RoutineScreen extends StatelessWidget {
+class RoutineScreen extends StatefulWidget {
   static const routeName = '/routine';
   final List<Exercise> exercises;
 
   const RoutineScreen(this.exercises, {super.key});
 
   @override
+  State<RoutineScreen> createState() => _RoutineScreenState();
+}
+
+class _RoutineScreenState extends State<RoutineScreen> {
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        // ProgressBar
+        // Exercise
+        // Stimulus Chips
+        // Countdown-Wrapper
+        );
   }
 }
